09ba791f3d9438efecfbccf2e20c74c4051cdc9a
Iryna Churilova <churilovairyna@gmail.com>
2021-01-30 00:09:35 +0800

    add exercises chapter

---
+++ documentation_again/default_arguments_in_the_middle.rb
@@ -0,0 +1,13 @@
+# def my_method(a, b = 2, c = 3, d)
+#   p [a, b, c, d]
+# end
+
+# my_method(4, 5, 6)
+
+#[4, 5, 3, 6]
+
+def ira_method(a, b = 4, c = 6, d)
+  p [a, b, c, d]
+end
+
+ira_method(8, 1, 7)
