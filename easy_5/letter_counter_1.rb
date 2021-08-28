f81da5ff8a527521cf92674a35d6e1b3eed787b2
IrynaNelson <churilovairyna@gmail.com>
2021-08-28 11:06:40 +0800

    add easy_5 exerecises

--- easy_5/letter_counter_1.rb
+++ easy_5/letter_counter_1.rb
@@ -1,4 +1,18 @@
-word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 1, 6 => 1 }
-word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 1, 7 => 2 }
-word_sizes("What's up doc?") == { 6 => 1, 2 => 1, 4 => 1 }
-word_sizes('') == {}
+def word_sizes(str_input)
+    count = {}
+    str_array = str_input.split(" ")
+    for word in str_array do
+        word_length = word.length
+        if count.has_key?(word_length)
+            count[word_length] += 1
+        else
+            count[word_length] = 1
+        end
+    end
+    return count
+end
+
+puts word_sizes('Four score and seven.') == { 3 => 1, 4 => 1, 5 => 1, 6 => 1 }
+puts word_sizes('Hey diddle diddle, the cat and the fiddle!') == { 3 => 5, 6 => 1, 7 => 2 }
+puts word_sizes("What's up doc?") == { 6 => 1, 2 => 1, 4 => 1 }
+puts word_sizes('') == {}
